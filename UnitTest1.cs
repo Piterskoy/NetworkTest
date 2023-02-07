906cf4fa6faeaadb81dd75e2e38ba58988e7e46c
Peter.G <piteskou@gmail.com>
2023-02-07 12:51:54 -0500

    fix ipv4 method get

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,23 +1,33 @@
 using System.Text;
 using CliWrap;
+using Xunit.Abstractions;
 
 namespace Network.Test;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
-    {
+    private readonly ITestOutputHelper output;
 
+    public UnitTest1(ITestOutputHelper output)
+    {
+        this.output = output;
+    }
+
+    [Fact]
+    public async Task Test1()
+    {
+        var method = await GetIp4Method("Wired connection 1");
+        output.WriteLine($"method={method}");
     }
 
     public async Task<string> GetIp4Method(string connection)
     {
         var stdOutBuffer = new StringBuilder();
-        var result = await Cli.Wrap("sudo")        
-        .WithArguments($"nmcli -f ipv4.method con show '{connection}'")        
-        .WithStandardOutputPipe(PipeTarget.ToStringBuilder(stdOutBuffer))    
+        var result = await Cli.Wrap("sudo")
+        .WithArguments($"nmcli -f ipv4.method con show '{connection}'")
+        .WithStandardOutputPipe(PipeTarget.ToStringBuilder(stdOutBuffer))
         .ExecuteAsync();
+
         return stdOutBuffer.ToString();
     }
     
