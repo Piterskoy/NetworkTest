1699117a5e601f32b6696f13740a715c545e558a
Peter.G <piteskou@gmail.com>
2023-02-07 12:09:21 -0500

    get ip4 method

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,3 +1,6 @@
+using System.Text;
+using CliWrap;
+
 namespace Network.Test;
 
 public class UnitTest1
@@ -7,4 +10,15 @@ public class UnitTest1
     {
 
     }
+
+    public async Task<string> GetIp4Method(string connection)
+    {
+        var stdOutBuffer = new StringBuilder();
+        var result = await Cli.Wrap("sudo")        
+        .WithArguments($"nmcli -f ipv4.method con show '{connection}'")        
+        .WithStandardOutputPipe(PipeTarget.ToStringBuilder(stdOutBuffer))    
+        .ExecuteAsync();
+        return stdOutBuffer.ToString();
+    }
+    
 }
